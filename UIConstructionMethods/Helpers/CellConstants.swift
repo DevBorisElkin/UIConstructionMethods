65b2374414c0b9ae5deae99f91c82de5cd850c16
DevBorisElkin <ea.foreverrr@gmail.com>
2022-08-01 20:11:03 +0300

    small backup

--- UIConstructionMethods/Helpers/CellConstants.swift
+++ UIConstructionMethods/Helpers/CellConstants.swift
@@ -10,22 +10,18 @@ import UIKit
 
 struct CellConstants {
     
-    static let borderYHeight: CGFloat = 5
-    static let itemsYOffset: CGFloat = 6
-    //static let generalItemHeight: CGFloat = 31
-    static let descriptionTopPosition: CGFloat = 98
+    static let borderYHeight: CGFloat = 5 // top/bottom purple borders height
+    static let itemsYOffset: CGFloat = 6 // offset from top/bottom border to an element
     
-    static let postLabelFont = UIFont.systemFont(ofSize: 15)
+    static let postLabelFont = UIFont.systemFont(ofSize: 15) // main font data
     
-    // offset by 16 from left and right for text
-    static let descriptionXOffset: CGFloat = 16
+    static let extraTextSpace: CGFloat = 3 // 3
     
+    // function to get screen width ?? TODO rework
     static func getScreenWidth() -> CGFloat {
         //return min(UIScreen.main.bounds.width, UIScreen.main.bounds.height)
         return UIScreen.main.bounds.width
     }
-    
-    // final version for description text
-    
+    // basic insets data for description text, like repo description, offsets from edges by X, bottom offset is not used
     static let descriptionTextInsets = UIEdgeInsets(top: 98, left: 16, bottom: 777, right: 16)
 }
